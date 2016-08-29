cfd7e2de784dc2b9df2b535a9045161750be4b19
Taiwen Jin <kimdaxterkid@gmail.com>
2016-08-29 13:50:54 -0400

    Version with HTTP request, but still need more to deal with the response from Server

--- FitEx/ViewController.swift
+++ FitEx/ViewController.swift
@@ -28,8 +28,39 @@ class ViewController: UIViewController {
         self.resignFirstResponder()
     }
     @IBAction func buttonLogin(sender: AnyObject) {
+        let jsondata = "[\"username\":\"" + textUsername.text! + "\",\"password\":\"" + textPassword.text! + "\"]"
+        print("My json data :\(jsondata)")
         textUsername.text = "deal with JSON data"
+        // prepare json data
+        let json = [ "username":textUsername.text! , "password": textPassword.text! ]
+        // create post request
+        do {
+            let url = NSURL(string: "http://128.173.239.215/login")!
+            let request = NSMutableURLRequest(URL: url)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.HTTPMethod = "POST"
+            let jsonData = try NSJSONSerialization.dataWithJSONObject(json, options: .PrettyPrinted)
+            print(jsonData)
+            // insert json data to the request
+            request.HTTPBody = jsonData
+            let task = NSURLSession.sharedSession().dataTaskWithRequest(request){ data, response, error in
+                    if error != nil{
+                        print("Error -> \(error)")
+                        return
+                    }
+                    do {
+                        let result = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? [String:AnyObject]
+                    
+                        print("Result -> \(result)")
+                    
+                    } catch {
+                        print("Error -> \(error)")
+                    }
+                }
+            task.resume()
+        } catch {
+            print("jsonData error")
+        }
     }
-    
 }
 
